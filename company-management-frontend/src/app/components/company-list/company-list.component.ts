3fdfb62325b1ae9a09ff2ffafdad5916586186d3
Mubarak <mubarakali.00@gmail.com>
2024-02-09 18:49:56 +0800

    chore: Update the company list screen.

--- company-management-frontend/src/app/components/company-list/company-list.component.ts
+++ company-management-frontend/src/app/components/company-list/company-list.component.ts
@@ -2,33 +2,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild, effect, signal } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Router, RouterLink } from '@angular/router';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { BackendService } from '../../services/backend.service';
+
 import { CompanyDto } from '../../utils/objects/Company';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
-import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { BackendService } from '../../services/backend.service';
 import { CompanySearchFilterComponent } from '../company-search-filter/company-search-filter.component';
 
 @Component({
   selector: 'app-company-list',
   standalone: true,
-  imports: [HttpClientModule, MatTableModule, MatButtonModule, RouterLink, MatSortModule, MatProgressSpinnerModule, MatPaginatorModule, CompanySearchFilterComponent],
+  imports: [HttpClientModule, RouterLink, CompanySearchFilterComponent],
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.scss'
 })
-export class CompanyListComponent implements AfterViewInit, OnInit, OnDestroy {
+export class CompanyListComponent implements OnInit, OnDestroy {
 
   sortByCompanyNameDesc = false;
   totalCount = 0;
   isLoadingResults = true;
   unsubDdlList!: Subscription;
-  displayedColumns: string[] = ['companyNo', 'companyName', 'industryName', 'totalEmployees', 'city', 'parentCompany', 'level'];
-  dataSource = new MatTableDataSource<CompanyDto>(undefined);
-  
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  // displayedColumns: string[] = ['companyNo', 'companyName', 'industryName', 'totalEmployees', 'city', 'parentCompany', 'level'];
+  items: CompanyDto[];
   
   companyNo = signal<number|undefined>(undefined);
   companyName = signal<string|undefined>(undefined);
@@ -38,6 +31,7 @@ export class CompanyListComponent implements AfterViewInit, OnInit, OnDestroy {
   parentCompany = signal<string|undefined>(undefined);
 
   constructor(private backendService: BackendService, private router: Router) {
+    this.items = [];
     effect(() => {
       this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany());
     })
@@ -50,19 +44,13 @@ export class CompanyListComponent implements AfterViewInit, OnInit, OnDestroy {
   ngOnDestroy() {
     console.log(" ngOnDestroyed called!")
     if (this.unsubDdlList) this.unsubDdlList.unsubscribe();
-    this.dataSource.disconnect();
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
   }
 
   getCompanyList(companyNo?: number, companyName?: string, industryId?: number, city?: string, totalEmployees?: number, parentCompany?: string, pageIndex = 1, pageSize = 5) {
     this.unsubDdlList = this.backendService.getCompanyList({ sortByCompanyNameDesc: this.sortByCompanyNameDesc, pageIndex, pageSize, 
               companyNo, companyName, industryId, city, parentCompany, totalEmployees })
       .subscribe(list => {
-        this.dataSource = new MatTableDataSource<CompanyDto>(list.items);
+        this.items = list.items;
         this.totalCount = list.totalPages;
         this.isLoadingResults = false;
         return list;
@@ -73,21 +61,21 @@ export class CompanyListComponent implements AfterViewInit, OnInit, OnDestroy {
     this.router.navigate(["/save", { companyId: row.id }]);
   }
 
-  announceSortChange(sortState: Sort) {
-    this.isLoadingResults = true;
-    if (sortState.direction === "asc") {
-      this.sortByCompanyNameDesc = false;
-      this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany());
-    } else {
-      this.sortByCompanyNameDesc = true;
-      this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany());
-    }
-  }
+  // announceSortChange(sortState: Sort) {
+  //   this.isLoadingResults = true;
+  //   if (sortState.direction === "asc") {
+  //     this.sortByCompanyNameDesc = false;
+  //     this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany());
+  //   } else {
+  //     this.sortByCompanyNameDesc = true;
+  //     this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany());
+  //   }
+  // }
 
-  pageChanged(event: PageEvent) {
-    console.log({ event });
-    this.isLoadingResults = true;
-    this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany(), ++event.pageIndex, event.pageSize);
-  }
+  // pageChanged(event: PageEvent) {
+  //   console.log({ event });
+  //   this.isLoadingResults = true;
+  //   this.getCompanyList(this.companyNo(), this.companyName(), this.industryId(), this.city(), this.totalEmployees(), this.parentCompany(), ++event.pageIndex, event.pageSize);
+  // }
 
 }
